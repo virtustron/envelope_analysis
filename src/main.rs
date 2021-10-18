9fb4191a2b09824e18763329493e87ddb41dafdc
virtustron <virtustron@gmail.com>
2021-10-18 16:05:13 +0300

    Fix container mutability

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@ use std::ptr;
 mod cpp_envelopes_analysis;
 
 fn main() {
-    //let container: *mut ::std::os::raw::c_void;
-    let container: *mut *mut std::os::raw::c_void;
+    //let container: *mut *mut std::os::raw::c_void;
+    let mut container: *mut std::os::raw::c_void;
     container = ptr::null_mut();
 
     unsafe{
-        if cpp_envelopes_analysis::InitializeEnvelopesContainer(container) == cpp_envelopes_analysis::INIT_SUCCEDED {
+        if cpp_envelopes_analysis::InitializeEnvelopesContainer(&mut container) == cpp_envelopes_analysis::INIT_SUCCEDED {
             let can_contain: *mut bool = ptr::null_mut();
             *can_contain = false;
 
-            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(*container, can_contain) == cpp_envelopes_analysis::COMPARATION_SUCCEDED {
+            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(container, can_contain) == cpp_envelopes_analysis::COMPARATION_SUCCEDED {
                 
                 if *can_contain {
                     println!("Can contain!");
