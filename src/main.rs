1735f7fde002cb1870474cb684341a742a1e34e5
virtustron <virtustron@gmail.com>
2021-10-18 17:02:40 +0300

    Change command line args processing to vec<string>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod cpp_envelopes_analysis;
 
 fn main() {
     
-    let mut command_line_arguments = std::env::args();
+    let command_line_arguments: Vec<String> = std::env::args().collect();
 
     if command_line_arguments.len() != 5
     {
@@ -16,11 +16,10 @@ fn main() {
         return;
     }
 
-    command_line_arguments.next();    
-    let arg_envelope1_size1 = command_line_arguments.next().unwrap();
-    let arg_envelope1_size2 = command_line_arguments.next().unwrap();
-    let arg_envelope2_size1 = command_line_arguments.next().unwrap();
-    let arg_envelope2_size2 = command_line_arguments.next().unwrap();
+    let arg_envelope1_size1 = &command_line_arguments[1];
+    let arg_envelope1_size2 = &command_line_arguments[2];
+    let arg_envelope2_size1 = &command_line_arguments[3];
+    let arg_envelope2_size2 = &command_line_arguments[4];
 
     let envelope1_size1 = arg_envelope1_size1.parse::<f64>().unwrap_or_else(|_| {
         println!("Not valid envelope size: {}.", arg_envelope1_size1);
