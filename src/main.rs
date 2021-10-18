c25b7641da1597f497f1f54357946f0b1041c185
virtustron <virtustron@gmail.com>
2021-10-18 13:54:50 +0300

    Use cpp code - in progress

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+mod cpp_envelopes_analysis;
+
 fn main() {
+    let container: *mut ::std::os::raw::c_void;
+
+    unsafe{
+        if cpp_envelopes_analysis::InitializeEnvelopesContainer(container) == cpp_envelopes_analysis::INIT_SUCCEDED {
+            let mut can_contain: *mut bool;
+
+            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(container, can_contain) == 0 {
+                
+            }
+        }
+    }
+
+    /*
+	void* container = NULL;
+	
+	// 10.0, 10.0, 15.0, 5.0
+	if (InitializeEnvelopesContainer(&container) == INIT_SUCCEDED)
+	{
+		bool can_contain;
+		
+		if (CanOneEnvelopeContainAnother(container, &can_contain));
+	
+		if (can_contain)
+		{
+			std::cout << "Can contain" << "\n";
+		}
+		else
+		{
+			std::cout << "Can not contain" << "\n";
+		}
+
+	}
+
+	if (container != NULL)
+	{
+		delete container;
+	} 
+
+
+    */
+
+
+
     println!("Hello, world!");
 }
