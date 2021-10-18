fa4b5d48b2ad10f8540844496fea78e50e21ce3c
virtustron <virtustron@gmail.com>
2021-10-18 16:14:38 +0300

    Fix bug can_contain

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
 
     unsafe{
         if cpp_envelopes_analysis::InitializeEnvelopesContainer(&mut container) == cpp_envelopes_analysis::INIT_SUCCEDED {
-            let can_contain: *mut bool = ptr::null_mut();
-            *can_contain = false;
+            let mut can_contain: bool;
+            can_contain = false;
 
-            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(container, can_contain) == cpp_envelopes_analysis::COMPARATION_SUCCEDED {
+            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(container, &mut can_contain) == cpp_envelopes_analysis::COMPARATION_SUCCEDED {
                 
-                if *can_contain {
+                if can_contain {
                     println!("Can contain!");
                 }
                 else {
