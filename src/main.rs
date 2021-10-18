cf545a534eed02bff955c9f17c7ea8a3ffcd7bd9
virtustron <virtustron@gmail.com>
2021-10-18 15:51:24 +0300

    Fixin bug with pointers

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,28 @@
+use std::ptr;
+
 mod cpp_envelopes_analysis;
 
 fn main() {
-    let container: *mut ::std::os::raw::c_void;
+    //let container: *mut ::std::os::raw::c_void;
+    let container: *mut *mut std::os::raw::c_void;
+    container = ptr::null_mut();
 
     unsafe{
         if cpp_envelopes_analysis::InitializeEnvelopesContainer(container) == cpp_envelopes_analysis::INIT_SUCCEDED {
-            let mut can_contain: *mut bool;
+            let can_contain: *mut bool = ptr::null_mut();
+            *can_contain = false;
 
-            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(container, can_contain) == 0 {
+            if cpp_envelopes_analysis::CanOneEnvelopeContainAnother(*container, can_contain) == cpp_envelopes_analysis::COMPARATION_SUCCEDED {
                 
+                if *can_contain {
+                    println!("Can contain!");
+                }
+                else {
+                    println!("Can not contain!");
+
+                }
             }
+            
         }
     }
-
-    /*
-	void* container = NULL;
-	
-	// 10.0, 10.0, 15.0, 5.0
-	if (InitializeEnvelopesContainer(&container) == INIT_SUCCEDED)
-	{
-		bool can_contain;
-		
-		if (CanOneEnvelopeContainAnother(container, &can_contain));
-	
-		if (can_contain)
-		{
-			std::cout << "Can contain" << "\n";
-		}
-		else
-		{
-			std::cout << "Can not contain" << "\n";
-		}
-
-	}
-
-	if (container != NULL)
-	{
-		delete container;
-	} 
-
-
-    */
-
-
-
-    println!("Hello, world!");
 }
